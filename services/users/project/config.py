5ed525afd78dd17f37eb2c74110c10252854649c
tanghaijun <tanghj@cyberpeace.cn>
2019-01-24 16:02:17 +0800

    docker and postgres

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 # coding=utf-8
+import os
+
 class Baseconfig:
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Baseconfig):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestingConfig(Baseconfig):
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 class ProductionConfig(Baseconfig):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
