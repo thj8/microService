1bc93f631578c3df17421d003d6af847c25e0c2e
tanghaijun <tanghj@cyberpeace.cn>
2019-01-31 10:03:01 +0800

    flask tool bar

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -4,9 +4,11 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension
 
 
 db = SQLAlchemy()
+toolbar = DebugToolbarExtension()
 
 
 def create_app(script_info=None):
@@ -20,6 +22,7 @@ def create_app(script_info=None):
 
     # set up extensions
     db.init_app(app)
+    toolbar.init_app(app)
 
     # register blueprints
     from project.api.users import users_blueprint
--- services/users/project/config.py
+++ services/users/project/config.py
@@ -7,10 +7,13 @@ class Baseconfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'my_precious'
+    DEBUG_TB_ENABLED = False
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
 
 
 class DevelopmentConfig(Baseconfig):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    DEBUG_TB_ENABLED = True
 
 
 class TestingConfig(Baseconfig):
