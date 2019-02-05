c4b28b4d68a6883f09d42ea07a6c9630a786b918
tanghaijun <tanghj@cyberpeace.cn>
2019-02-05 21:36:32 +0800

    part 3 flask jwt setup

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -6,10 +6,12 @@ import os
 class Baseconfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'my_precious'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     DEBUG_TB_ENABLED = False
     DEBUG_TB_INTERCEPT_REDIRECTS = False
     BCRYPT_LOG_ROUNDS = 13
+    TOKEN_EXPIRATION_DAYS = 30
+    TOKEN_EXPIRATION_SECONDS = 0
 
 
 class DevelopmentConfig(Baseconfig):
@@ -22,6 +24,8 @@ class TestingConfig(Baseconfig):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
     BCRYPT_LOG_ROUNDS = 4
+    TOKEN_EXPIRATION_DAYS = 0
+    TOKEN_EXPIRATION_SECONDS = 3
 
 
 class ProductionConfig(Baseconfig):
